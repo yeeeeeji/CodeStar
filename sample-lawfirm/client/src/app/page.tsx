da0ac08b0b4037dbcb92199eaeb8d8e5f000595d
hallo27 <hallo27@naver.com>
2025-05-26 00:51:45 +0900

    feat: 메인페이지 업무사례 구현

--- sample-lawfirm/client/src/app/page.tsx
+++ sample-lawfirm/client/src/app/page.tsx
@@ -1,11 +1,13 @@
-import BusinessStatsSection from './_components/business-stats/business-stats-section';
-import Carousel from './_components/carousel/carousel';
+import BusinessStatsSection from "./_components/business-stats/business-stats-section";
+import Carousel from "./_components/carousel/carousel";
+import CaseSection from "./_components/case-section/case-section";
 
 export default function Home() {
   return (
     <div>
       <Carousel />
       <BusinessStatsSection />
+      <CaseSection />
     </div>
   );
 }
