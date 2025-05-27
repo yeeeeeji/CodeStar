c014a4a656b331df81567c2ac524e535cb1c615d
hallo27 <hallo27@naver.com>
2025-05-27 23:33:50 +0900

    feat: 헤더 크기 조절 및 정렬

--- sample-lawfirm/client/src/component/header/header.tsx
+++ sample-lawfirm/client/src/component/header/header.tsx
@@ -86,37 +86,42 @@ export default function Header() {
   };
 
   return (
-    <div className="relative mx-30">
-      {/* 하위 메뉴 바탕 상자 */}
-      <div
-        className={`absolute w-full h-40 bg-black transition-all duration-300 ease-in-out transform ${
-          open
-            ? "translate-y-0 opacity-50"
-            : "-translate-y-10 opacity-0 pointer-events-none"
-        }`}
-      ></div>
-      <div className="absolute flex justify-between" style={{ width: "100%" }}>
-        <div>
-          <h1>로고</h1>
-        </div>
-        <div onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
-          <div className="flex justify-between w-120 text-white">
-            {categories.map((category) => (
-              <div key={category.id}>
-                <div className="my-3 border-b-2 border-transparent hover:border-indigo-600">
-                  {category.name}
+    <div className="flex flex-col items-center">
+      <div className="relative w-[1440px] z-999">
+        {/* 하위 메뉴 바탕 상자 */}
+        <div
+          className={`absolute w-full h-40 bg-black transition-all duration-300 ease-in-out transform ${
+            open
+              ? "translate-y-0 opacity-50"
+              : "-translate-y-10 opacity-0 pointer-events-none"
+          }`}
+        ></div>
+        <div
+          className="absolute flex justify-between mt-[24px]"
+          style={{ width: "100%" }}
+        >
+          <div>
+            <h1>로고</h1>
+          </div>
+          <div onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
+            <div className="flex justify-between w-120 text-white">
+              {categories.map((category) => (
+                <div key={category.id}>
+                  <div className="my-3 border-b-2 border-transparent hover:border-indigo-600">
+                    {category.name}
+                  </div>
+                  <div
+                    className={`absolute transition-opacity duration-300 ease-in-out delay-0 ${
+                      open ? "opacity-100" : "opacity-0 pointer-events-none"
+                    }`}
+                  >
+                    {category.items.map((item) => (
+                      <div key={item.id}>{item.name}</div>
+                    ))}
+                  </div>
                 </div>
-                <div
-                  className={`absolute transition-opacity duration-300 ease-in-out delay-0 ${
-                    open ? "opacity-100" : "opacity-0 pointer-events-none"
-                  }`}
-                >
-                  {category.items.map((item) => (
-                    <div key={item.id}>{item.name}</div>
-                  ))}
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
