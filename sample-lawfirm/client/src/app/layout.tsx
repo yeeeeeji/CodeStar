cdfb8787c2f79de1f5d522451399944e7e605e40
Yeoni <siunaus224@gmail.com>
2025-05-30 20:16:39 +0900

    fix: metadata 배포 / 로컬 분류 수정

--- sample-lawfirm/client/src/app/layout.tsx
+++ sample-lawfirm/client/src/app/layout.tsx
@@ -11,7 +11,9 @@ const pretendard = localFont({
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL(process.env.NODE_ENV === 'production' ? 'code-star.vercel.app' : 'http://localhost:3000'),
+  metadataBase: new URL(
+    process.env.NODE_ENV === 'production' ? 'https://code-star.vercel.app' : 'http://localhost:3000'
+  ),
   title: '법무법인 코드별',
   description: '신뢰를 중심으로 변화를 이끄는 로펌',
   openGraph: {
