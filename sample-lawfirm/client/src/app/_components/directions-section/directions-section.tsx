55db9a79109e7581ba7d1316bbef6c00412d1d29
hallo27 <hallo27@naver.com>
2025-06-02 23:16:22 +0900

    feat: 오시는 길 기본 구조 구현

---
+++ sample-lawfirm/client/src/app/_components/directions-section/directions-section.tsx
@@ -0,0 +1,31 @@
+import WorkAreaTitle from "../work-area-section/work-area-title";
+import { BuildingOfficeIcon } from "@heroicons/react/20/solid";
+import DirectionsContent from "./directions-content";
+
+export default function DirectionsSection() {
+  return (
+    <div className="flex justify-center">
+      <div className="w-[1440px] grid grid-cols-12">
+        <div className="col-span-5 px-[70px] pt-[70px] pb-[54px]">
+          <WorkAreaTitle
+            title="오시는 길"
+            position="left"
+            icon={BuildingOfficeIcon}
+          />
+          <div className="mt-[42px] mb-[35px]">
+            <DirectionsContent
+              title="주소"
+              content="경기도 수원시 팔달구 효원로 03번길1-25 효원빌딩 7층"
+            />
+            <div className="h-[25px]" />
+            <DirectionsContent title="우편번호" content="05183" />
+          </div>
+          <button className="border px-8 py-2 sm:px-12 sm:py-2.5 md:px-16 md:py-3 text-sm md:text-base lg:text-lg hover:bg-black hover:text-white transition-colors duration-300">
+            자세히보기
+          </button>
+        </div>
+        <div className="col-span-7">지도</div>
+      </div>
+    </div>
+  );
+}
