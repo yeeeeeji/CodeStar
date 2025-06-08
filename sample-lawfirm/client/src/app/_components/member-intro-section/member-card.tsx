f1f6dc5c73b513e00a968a08089c5b205ab20bca
hallo27 <hallo27@naver.com>
2025-06-08 22:25:57 +0900

    feat: 구성원소개 부분 반응형 디자인 적용

--- sample-lawfirm/client/src/app/_components/member-intro-section/member-card.tsx
+++ sample-lawfirm/client/src/app/_components/member-intro-section/member-card.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 interface MemberCardProps {
   img: string;
@@ -9,12 +9,21 @@ interface MemberCardProps {
 
 export default function MemberCard({ name, part, img }: MemberCardProps) {
   return (
-    <Link href={''}>
-      <div className='relative h-[514px]'>
-        <Image src={`/images/members/${img}.png`} alt={name} fill />
-        <div className='absolute right-0 bottom-0 mb-[20px] justify-items-end'>
-          <div className='pr-[20px] pb-[8px] text-white text-2xl font-bold'>{name} 변호사</div>
-          <div className='pl-[13px] pr-[20px] py-[8px] bg-codestar-dark-navy text-white text-2xl font-bold'>{part}</div>
+    <Link href={""}>
+      <div className="relative h-[514px] w-[345px] 2xl:mb-0 mb-3">
+        <Image
+          src={`/images/members/${img}.png`}
+          alt={name}
+          fill
+          className="object-cover"
+        />
+        <div className="absolute right-0 bottom-0 mb-[20px] justify-items-end">
+          <div className="pr-[20px] pb-[8px] text-white text-2xl font-bold">
+            {name} 변호사
+          </div>
+          <div className="pl-[13px] pr-[20px] py-[8px] bg-codestar-dark-navy text-white text-2xl font-bold">
+            {part}
+          </div>
         </div>
       </div>
     </Link>
--- sample-lawfirm/client/src/app/_components/member-intro-section/member-intro-section.tsx
+++ sample-lawfirm/client/src/app/_components/member-intro-section/member-intro-section.tsx
@@ -5,7 +5,7 @@ import { UserGroupIcon } from "@heroicons/react/20/solid";
 export default function MemberIntroSection() {
   return (
     <div className="flex justify-center my-[180px]">
-      <div className="w-[1440px] justify-center">
+      <div className="w-80 lg:w-[720px] 2xl:w-[1440px] justify-center">
         <div className="flex justify-center">
           <WorkAreaTitle
             title="구성원소개"
@@ -13,7 +13,14 @@ export default function MemberIntroSection() {
             icon={UserGroupIcon}
           />
         </div>
-        <div className="justify-between grid grid-cols-4 gap-4 mt-[85px]">
+        <div
+          className="
+            justify-between grid mt-[85px]
+            grid-cols-1
+            lg:grid-cols-2
+            2xl:grid-cols-4
+          "
+        >
           <MemberCard img="member1" name="이수희" part="의료제약그룹장" />
           <MemberCard img="member2" name="김민희" part="기업법무그룹장" />
           <MemberCard img="member3" name="이민우" part="채권추심그룹장" />
