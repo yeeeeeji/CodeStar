f89886899c53d95e0ec424184ef0edc4cc4b0166
hallo27 <hallo27@naver.com>
2025-06-25 01:18:19 +0900

    feat: 업무 사례 부분 Pagination에 현재 페이지 강조 스타일 적용

--- sample-lawfirm/client/src/components/button/PaginationArrowBtn.tsx
+++ sample-lawfirm/client/src/components/button/PaginationArrowBtn.tsx
@@ -1,23 +1,26 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 interface PaginationArrowBtnProps {
   right?: boolean;
+  current?: number;
 }
 
 export default function PaginationArrowBtn({
   right = false,
+  current = 1,
 }: PaginationArrowBtnProps) {
   const iconStyle = "size-5";
 
   return (
     <div className="border rounded-full w-[46px] h-[46px] flex items-center justify-center">
-      <div className="">
-        {!right ? (
-          <ChevronLeftIcon className={iconStyle} />
-        ) : (
+      <Link href={`/case?page=${right ? current + 1 : current - 1}`}>
+        {right ? (
           <ChevronRightIcon className={iconStyle} />
+        ) : (
+          <ChevronLeftIcon className={iconStyle} />
         )}
-      </div>
+      </Link>
     </div>
   );
 }
