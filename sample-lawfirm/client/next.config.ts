52996abed4fcc7da89fa16a412ba81ae9aa63e71
Yeoni <siunaus224@gmail.com>
2025-11-18 21:47:39 +0900

    design: 배너 사진 변경

--- sample-lawfirm/client/next.config.ts
+++ sample-lawfirm/client/next.config.ts
@@ -3,7 +3,16 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['i.imgur.com', 'imgur.com']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'imgur.com'
+      }
+    ]
   }
 };
 
