ff103349c2b9ae014c8df8ab86db873e7cca31b4
hallo27 <hallo27@naver.com>
2025-11-20 00:39:10 +0900

    feat: 업무 사례 필터 드롭다운 메뉴 기본 구조 생성

--- sample-lawfirm/client/src/app/case/_component/case-filter/case-filter.tsx
+++ sample-lawfirm/client/src/app/case/_component/case-filter/case-filter.tsx
@@ -1,12 +1,72 @@
+"use client";
+
 import { getCaseCategories } from "@/hooks/getCaseCategories";
 import DropdownFilter from "./dropdown-filter";
+import { useEffect, useRef, useState } from "react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
-export default async function CaseFilter() {
-  const categories = await getCaseCategories();
-  console.log(categories);
+export default function CaseFilter() {
+  const [options, setOptions] = useState<string[]>([]);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [selectedOption, setSelectedOption] = useState<string>("");
+
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const toggleDropdown = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const selectOption = (option: string) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    const loadCategories = async () => {
+      try {
+        const categories = await getCaseCategories();
+        setOptions(categories);
+      } catch (error) {
+        console.error("카테고리 로딩 중 오류 발생", error);
+      }
+    };
+
+    loadCategories();
+  });
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(e.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+      document.addEventListener("mousedown", handleClickOutside);
+
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    };
+  }, []);
+
   return (
-    <div className="flex">
-      <DropdownFilter title={categories} />
+    <div ref={dropdownRef} className="flex">
+      <div className="flex" onClick={toggleDropdown}>
+        <div className="text-base w-[100px]">{selectedOption}</div>
+        <div>
+          <ChevronDownIcon className="size-6" />
+        </div>
+      </div>
+      {isOpen && (
+        <div className="absolute mt-6">
+          {options.map((option) => (
+            <div key={option} onClick={() => selectOption(option)}>
+              {option}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
