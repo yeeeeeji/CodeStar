f8085eb0882d5f32e91e578f2cfcb24972ec2440
hallo27 <hallo27@naver.com>
2025-11-24 23:10:54 +0900

    fix: 자세히 보기 버튼 크기 불일치 문제 해결

--- sample-lawfirm/client/src/components/button/ViewMoreBtn.tsx
+++ sample-lawfirm/client/src/components/button/ViewMoreBtn.tsx
@@ -10,15 +10,16 @@ export default function ViewMoreBtn({
   black = false,
 }: ViewMoreBtnProps) {
   return (
-    <Link
-      href={path}
-      className={`border px-8 py-2 sm:px-12 sm:py-[18] md:px-16 md:py-[19.5] text-sm md:text-base lg:text-lg transition-colors duration-300 ${
-        black
-          ? `hover:bg-black hover:text-white`
-          : `border-white text-white hover:bg-white hover:text-black`
-      }`}
-    >
-      자세히보기
+    <Link href={path}>
+      <div
+        className={`border px-8 py-2 sm:px-12 sm:py-[18] md:px-16 md:py-[19.5] text-sm md:text-base lg:text-lg transition-colors duration-300 ${
+          black
+            ? `hover:bg-black hover:text-white`
+            : `border-white text-white hover:bg-white hover:text-black`
+        }`}
+      >
+        자세히 보기
+      </div>
     </Link>
   );
 }
