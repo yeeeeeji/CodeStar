9ba2dd8962e2fe29925883a95412d4fbbc39fd8a
hallo27 <hallo27@naver.com>
2025-12-01 21:54:56 +0900

    style: 메인 화면 오시는 길 모바일 버전 디자인 수정

--- sample-lawfirm/client/src/app/_components/directions-section/directions-content.tsx
+++ sample-lawfirm/client/src/app/_components/directions-section/directions-content.tsx
@@ -8,8 +8,10 @@ export default function DirectionsContent({
   content,
 }: DirectionsContentProps) {
   return (
-    <div className="flex justify-between text-lg xl:text-xl 2xl:text-2xl">
-      <div className="w-[83px] flex-shrink-0 2xl:mr-[30px]">{title}</div>
+    <div className="flex justify-between text-[12px] md:text-lg xl:text-xl 2xl:text-2xl">
+      <div className="w-[46px] mr-[20px] md:w-[83px] md:mr-0 flex-shrink-0 2xl:mr-[30px]">
+        {title}
+      </div>
       <div className="w-full">{content}</div>
     </div>
   );
--- sample-lawfirm/client/src/app/_components/directions-section/directions-section.tsx
+++ sample-lawfirm/client/src/app/_components/directions-section/directions-section.tsx
@@ -26,27 +26,19 @@ export default function DirectionsSection() {
 
   return (
     <div className="flex justify-center">
-      <div className="w-[1440px] grid grid-cols-12">
-        <div className="col-span-5 px-[70px] pt-[70px] pb-[54px]">
+      <div className="md:w-[1440px] md:grid md:grid-cols-12">
+        <div className="md:hidden flex justify-center mb-[31px]">
           <WorkAreaTitle
             title="오시는 길"
-            position="left"
+            position="center"
+            fontSize={16}
             icon={BuildingOfficeIcon}
           />
-          <div className="mt-[42px] mb-[35px]">
-            <DirectionsContent
-              title="주소"
-              content="경기도 수원시 팔달구 효원로 03번길1-25 효원빌딩 7층"
-            />
-            <div className="h-[25px]" />
-            <DirectionsContent title="우편번호" content="05183" />
-          </div>
-          <ViewMoreBtn black={true} path="/introduce#directions" />
         </div>
-        <div className="col-span-7 relative">
+        <div className="md:order-last col-span-7 relative">
           <div
             ref={mapContainerRef}
-            className="w-full h-full min-h-[400px]"
+            className="w-full h-full md:min-h-[400px] md:mx-0 min-h-[196px] mx-[18px]"
           ></div>
           <Script
             strategy="afterInteractive"
@@ -57,6 +49,26 @@ export default function DirectionsSection() {
               console.error("네이버 지도 스크립트 로드 실패:", e);
             }}
           />
+        </div>
+        <div className="md:order-first col-span-5 md:px-[70px] md:pt-[70px] md:pb-[54px]">
+          <div className="hidden md:block">
+            <WorkAreaTitle
+              title="오시는 길"
+              position="left"
+              icon={BuildingOfficeIcon}
+            />
+          </div>
+          <div className="mx-[22px] mt-[20px] mb-[30px] md:mt-[42px] md:mb-[35px]">
+            <DirectionsContent
+              title="주소"
+              content="경기도 수원시 팔달구 효원로 03번길1-25 효원빌딩 7층"
+            />
+            <div className="h-[7px] md:h-[25px]" />
+            <DirectionsContent title="우편번호" content="05183" />
+          </div>
+          <div className="hidden md:block">
+            <ViewMoreBtn black={true} path="/introduce#directions" />
+          </div>
         </div>
       </div>
     </div>
