db753952bddafa46c4c224083e688c4d87250916
hallo27 <hallo27@naver.com>
2025-12-01 22:45:48 +0900

    style: 메인 화면 업무사례 모바일 버전 디자인 수정

--- sample-lawfirm/client/src/components/button/ViewMoreBtn.tsx
+++ sample-lawfirm/client/src/components/button/ViewMoreBtn.tsx
@@ -1,16 +1,21 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 interface ViewMoreBtnProps {
   path?: string;
   black?: boolean;
 }
 
-export default function ViewMoreBtn({ path = '#', black = false }: ViewMoreBtnProps) {
+export default function ViewMoreBtn({
+  path = "#",
+  black = false,
+}: ViewMoreBtnProps) {
   return (
     <Link href={path}>
       <div
-        className={`inline-flex items-center justify-center w-[100px] h-[25px] md:w-40 md:h-12 lg:w-48 lg:h-14 border text-[10px] md:text-sm lg:text-base transition-colors duration-300 ${
-          black ? `hover:bg-black hover:text-white` : `border-white text-white hover:bg-white hover:text-black`
+        className={`inline-flex items-center justify-center w-[100px] h-[25px] sm:w-40 sm:h-12 lg:w-48 lg:h-14 border text-[10px] sm:text-sm lg:text-base transition-colors duration-300 ${
+          black
+            ? `hover:bg-black hover:text-white`
+            : `border-white text-white hover:bg-white hover:text-black`
         }`}
       >
         자세히 보기
