e2ebe5ff5da9f4ce60d17eb263839d9b1832e366
hallo27 <hallo27@naver.com>
2025-12-03 16:02:09 +0900

    style: 법인소개 오시는 길 부분 반응형 디자인 적용

--- sample-lawfirm/client/src/app/introduce/_components/directions/directions-content.tsx
+++ sample-lawfirm/client/src/app/introduce/_components/directions/directions-content.tsx
@@ -2,51 +2,61 @@ import Image from "next/image";
 
 export default function DirectionsContent() {
   return (
-    <div className="flex justify-between my-[50px]">
-      <div className="flex w-[588px]">
+    <div className="sm:flex sm:justify-between my-[50px] mx-[17px] sm:mx-0">
+      <div className={contentStyle}>
         <ImgCircle imgSrc="/images/map/bus.svg" />
-        <div>
-          <p className="text-[32px] font-bold mb-[20px]">버스 이용 시</p>
+        <div className="w-full">
+          <MainTitle title="버스 이용 시" />
           <div>
-            <div className="grid text-[24px] leading-normal gap-y-[40px]">
+            <div className="grid leading-normal gap-y-[10px] sm:gap-y-[40px]">
               <div>
                 <BoldTitle title="수원시청 정류장 하차" />
-                <div className="grid gap-y-1">
-                  <p>일반버스: 82-1, 85, 92, 720-2</p>
-                  <p>직행좌석버스: 3002, 4000</p>
-                  <p>→ 하차 후 도보 약 5분 (효원로 3번길 방향)</p>
-                </div>
+                <TextBlock
+                  contents={[
+                    "일반버스: 82-1, 85, 92, 720-2",
+                    "직행좌석버스: 3002, 4000",
+                    "→ 하차 후 도보 약 5분 (효원로 3번길 방향)",
+                  ]}
+                />
               </div>
+              <Border />
               <div>
                 <BoldTitle title="팔달구청 정류장 하차" />
-                <div className="grid gap-y-1">
-                  <p>일반버스: 13-1, 27, 36</p>
-                  <p>→ 하차 후 효원로 방향 도보 3분</p>
-                </div>
+                <TextBlock
+                  contents={[
+                    "일반버스: 13-1, 27, 36",
+                    "→ 하차 후 효원로 방향 도보 3분",
+                  ]}
+                />
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="flex w-[588px]">
+      <div className={`${contentStyle} mt-[38px] sm:mt-0`}>
         <ImgCircle imgSrc="/images/map/subway.svg" />
-        <div>
-          <p className="text-[32px] font-bold mb-[20px]">지하철 이용 시</p>
+        <div className="w-full">
+          <MainTitle title="지하철 이용 시" />
           <div>
-            <div className="grid text-[24px] leading-normal gap-y-[40px]">
+            <div className="grid leading-normal gap-y-[10px] sm:gap-y-[40px]">
               <div>
                 <BoldTitle title="1호선 수원역 하차 후" />
-                <div className="grid gap-y-1">
-                  <p>→ 4번 출구에서 나와</p>
-                  <p>버스 또는 택시 환승 (약 10분 소요)</p>
-                </div>
+                <TextBlock
+                  contents={[
+                    "→ 4번 출구에서 나와",
+                    "버스 또는 택시 환승 (약 10분 소요)",
+                  ]}
+                />
               </div>
+              <Border />
               <div>
                 <BoldTitle title="분당선 수원시청역 하차 후" />
-                <div className="grid gap-y-1">
-                  <p>→ 10번 출구 도보 약 8분</p>
-                  <p>→ 효원로 3번길 방면으로 직진, 효원빌딩 7층</p>
-                </div>
+                <TextBlock
+                  contents={[
+                    "→ 10번 출구 도보 약 8분",
+                    "→ 효원로 3번길 방면으로 직진, 효원빌딩 7층",
+                  ]}
+                />
               </div>
             </div>
           </div>
@@ -55,6 +65,8 @@ export default function DirectionsContent() {
     </div>
   );
 }
+
+const contentStyle = "flex sm:w-[588px]";
 
 interface ImgCircleProps {
   imgSrc: string;
@@ -65,7 +77,8 @@ const ImgCircle = ({ imgSrc }: ImgCircleProps) => {
     <div
       className={`
         relative overflow-hidden rounded-full bg-gray-100 flex-shrink-0
-        w-[60px] h-[60px] mr-[30px]
+        w-[40px] h-[40px] mr-[11px]
+        sm:w-[60px] sm:h-[60px] sm:mr-[30px]
         xl:w-[80px] xl:h-[80px] xl:mr-[40px]
         2xl:w-[102px] 2xl:h-[102px] 2xl:mr-[50px]
       `}
@@ -73,7 +86,10 @@ const ImgCircle = ({ imgSrc }: ImgCircleProps) => {
       <div
         className={`
           absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
-          w-[40px] h-[40px] xl:w-[50px] xl:h-[50px] 2xl:w-[70px] 2xl:h-[70px]
+          w-[30px] h-[30px] 
+          sm:w-[40px] sm:h-[40px] 
+          xl:w-[50px] xl:h-[50px] 
+          2xl:w-[70px] 2xl:h-[70px]
         `}
       >
         <Image src={imgSrc} alt="오시는 길" fill className="object-contain" />
@@ -82,10 +98,42 @@ const ImgCircle = ({ imgSrc }: ImgCircleProps) => {
   );
 };
 
-interface BoldTitleProps {
+interface TitleProps {
   title: string;
 }
 
-const BoldTitle = ({ title }: BoldTitleProps) => {
-  return <p className="font-bold mb-[12px]">{title}</p>;
+const MainTitle = ({ title }: TitleProps) => {
+  return (
+    <p className="font-bold text-[16px] mb-[10px] sm:text-[32px] sm:mb-[20px]">
+      {title}
+    </p>
+  );
+};
+
+const BoldTitle = ({ title }: TitleProps) => {
+  return (
+    <p className="font-bold text-blue-600 text-[12px] mb-[6px] sm:text-[24px] sm:mb-[12px]">
+      {title}
+    </p>
+  );
+};
+
+interface TextBlockProps {
+  contents: string[];
+}
+
+const TextBlock = ({ contents }: TextBlockProps) => {
+  return (
+    <div className="grid gap-y-1">
+      {contents.map((c) => (
+        <p key={c} className="text-[12px] sm:text-[24px]">
+          {c}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+const Border = () => {
+  return <div className="sm:hidden w-full border-t border-gray-600"></div>;
 };
