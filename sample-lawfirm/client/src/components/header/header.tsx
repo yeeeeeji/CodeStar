f8483fb898db2bd8625908e21e05c29161b51396
Elise <siunaus224@gmail.com>
2025-12-03 14:12:07 +0900

    design: 헤더 모바일 버전 추가

--- sample-lawfirm/client/src/components/header/header.tsx
+++ sample-lawfirm/client/src/components/header/header.tsx
@@ -3,6 +3,7 @@
 // import { useState } from "react";
 import Link from 'next/link';
 import Image from 'next/image';
+import { useState } from 'react';
 
 const categories = [
   {
@@ -68,6 +69,8 @@ const categories = [
 export default function Header() {
   // const [open, setOpen] = useState(false);
 
+  const [mobileOpen, setMobileOpen] = useState(false);
+
   // const handleMouseOver = () => {
   //   setOpen(true);
   // };
@@ -86,7 +89,7 @@ export default function Header() {
               : "-translate-y-10 opacity-0 pointer-events-none"
           }`}
         /> */}
-        <div className='md:absolute flex justify-between h-[96px]' style={{ width: '100%' }}>
+        <div suppressHydrationWarning className='hidden md:absolute md:flex justify-between h-[96px]' style={{ width: '100%' }}>
           <div className='my-auto px-[20px]'>
             <Link href='/'>
               <Image src='/images/logo/logo.svg' alt='logo' width={160} height={38} />
@@ -120,6 +123,45 @@ export default function Header() {
             </div>
           </div>
         </div>
+
+        {/* 모바일 */}
+        <div className='md:hidden flex justify-between items-center h-[62px] bg-white w-full pl-4 relative z-10'>
+          <Link href='/'>
+            <Image src='/images/logo/logo-navy.svg' alt='logo' width={120} height={30} />
+          </Link>
+        </div>
+
+        <button
+          className='md:hidden w-[62px] h-[62px] bg-codestar-dark-navy flex items-center justify-center fixed top-0 right-0 z-30'
+          onClick={() => setMobileOpen(!mobileOpen)}
+        >
+          {mobileOpen ? (
+            <Image src='/icons/close.svg' alt='close' width={24} height={24} />
+          ) : (
+            <Image src='/images/menu/menu.svg' alt='menu' width={24} height={24} />
+          )}
+        </button>
+
+        {mobileOpen && (
+          <div className='md:hidden absolute top-0 left-0 w-full h-[500px] px-10 py-[74px] bg-codestar-dark-navy z-20'>
+            <div className='flex flex-col justify-between h-full'>
+              <div>
+                {categories.map((category) => (
+                  <div key={category.id} className='py-3 border-b border-white last:border-b-0'>
+                    <Link href={category.href} className='text-2xl font-semibold text-white'>
+                      {category.name}
+                    </Link>
+                  </div>
+                ))}
+              </div>
+
+              <div>
+                <p className='text-lg font-semibold text-white'>코드별</p>
+                <p className='text-lg font-semibold text-white mt-2'>회원약관</p>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
