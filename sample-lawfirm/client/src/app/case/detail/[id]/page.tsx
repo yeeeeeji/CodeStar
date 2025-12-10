0b980bac4d0bc913fca329d393291a3e8d0bd7d5
hallo27 <hallo27@naver.com>
2025-12-10 12:25:40 +0900

    design: 업무사례 상세페이지 모바일 버전 추가

--- sample-lawfirm/client/src/app/case/detail/[id]/page.tsx
+++ sample-lawfirm/client/src/app/case/detail/[id]/page.tsx
@@ -3,6 +3,7 @@ import { fetchCase } from "@/lib/db/cases/api";
 import { format } from "date-fns";
 import { Suspense } from "react";
 import { ClockIcon } from "@heroicons/react/24/outline";
+import Breadcrumb from "@/components/layout/breadcrumb";
 
 interface CaseDetailProps {
   params: Promise<{
@@ -16,30 +17,33 @@ export default async function CaseDetailPage({ params }: CaseDetailProps) {
 
   return (
     <div>
-      <Banner imageAlt="업무사례 배너" title="업무사례" />
+      <Breadcrumb title="업무사례" />
+      <div className="hidden sm:block">
+        <Banner imageAlt="업무사례 배너" title="업무사례" />
+      </div>
       <div className="flex justify-center">
-        <div className="w-full sm:w-[1440px]">
+        <div className="w-full mx-[30px] sm:w-[1440px] sm:mx-0">
           <Suspense>
             {caseResult === undefined ? (
               <div></div>
             ) : (
               <div className="mt-24 mb-24">
                 <div>
-                  <div className="text-[48px] font-bold">
+                  <div className="font-bold text-[28px] sm:text-[48px]">
                     {caseResult.title}
                   </div>
-                  <div className="mt-6 mb-8 text-[18px]">
+                  <div className="mt-4 mb-6 text-[12px] sm:mt-6 sm:mb-8 sm:text-[18px]">
                     <div>{`분류 > ${caseResult.category}`}</div>
                     <div className="flex text-gray-400 mt-1">
-                      <ClockIcon className="w-4 h-4 mr-1 my-auto" />
+                      <ClockIcon className="w-3 h-3 sm:w-4 sm:h-4 mr-1 my-auto" />
                       <div>
                         {format(caseResult.createdAt, "yy-MM-dd HH:mm")}
                       </div>
                     </div>
                   </div>
-                  <div className="w-full mb-12 border-t border-gray-200"></div>
+                  <div className="w-full mb-8 sm:mb-12 border-t border-gray-200"></div>
                 </div>
-                <div className="text-[24px]/12 whitespace-pre-wrap">
+                <div className="text-[16px]/8 sm:text-[24px]/12 whitespace-pre-wrap">
                   {caseResult.content}
                 </div>
                 <div className="w-full my-12 border-t border-gray-200"></div>
